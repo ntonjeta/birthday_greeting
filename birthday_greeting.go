56e0be7d84d7734b1f6abc5ca55e8603e0d79c0a
ntonjeta <ntonjeta@autistici.org>
2025-01-11 21:10:18 +0100

    add first dummy test

--- birthday_greeting.go
+++ birthday_greeting.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Friend struct {
+	name    string
+	surname string
+}
+
+func greeting(_ []Friend, _ time.Time) (string, error) {
+	return "", errors.New("todo")
+}
 
 func main() {
 	fmt.Printf("Birthday greeting! \n")
--- birthday_greeting_test.go
+++ birthday_greeting_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var friends = []Friend{
+	{name: "Mary", surname: "Ann"},
+	{name: "John", surname: "Doe"},
+}
 
 func TestAcceptance(t *testing.T) {
-	main()
+	date := time.Now()
+
+	var mail, e = greeting(friends, date)
+
+	assert.NotNil(t, e)
+	assert.Empty(t, mail)
 }
