580c7566b713e1e00ce240fd817294e976d8d968
ntonjeta <ntonjeta@autistici.org>
2025-01-11 20:39:15 +0100

    add main birthday greeting

---
+++ birthday_greeting.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Birthday greeting! \n")
+}
---
+++ birthday_greeting_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestAcceptance(t *testing.T) {
+	main()
+}
