4d1b093cb5e83bf6e44609f27031d3c91cd776c3
ntonjeta <ntonjeta@autistici.org>
2025-01-11 21:39:35 +0100

    add john birthday test and simple implementation

--- birthday_greeting.go
+++ birthday_greeting.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type Friend struct {
-	name    string
-	surname string
+	name     string
+	surname  string
+	birthday time.Time
 }
 
-func greeting(_ []Friend) (string, error) {
-	return "Subject: Happy birthday!\nHappy birthday, dear Mary!", nil
+func greeting(friends []Friend, date time.Time) (string, error) {
+
+	for _, f := range friends {
+		if isBirthday(f, date) {
+			return fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!", f.name), nil
+		}
+	}
+	return " ", nil // TODO erros ?
+}
+
+func isBirthday(friend Friend, date time.Time) bool {
+	return friend.birthday.Day() == date.Day() &&
+		friend.birthday.Month() == date.Month()
 }
 
 func main() {
--- birthday_greeting_test.go
+++ birthday_greeting_test.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var friends = []Friend{
-	{name: "Mary", surname: "Ann"},
-	{name: "John", surname: "Doe"},
+	{name: "Mary", surname: "Ann", birthday: time.Date(1974, 11, 21, 0, 0, 0, 0, time.Local)},
+	{name: "John", surname: "Doe", birthday: time.Date(1974, 10, 1, 0, 0, 0, 0, time.Local)},
 }
 
-func TestAcceptance(t *testing.T) {
-	var mail, e = greeting(friends)
+func TestAcceptance_MaryBirthday(t *testing.T) {
+	var date = time.Date(2024, 11, 21, 0, 0, 0, 0, time.Local)
+
+	var mail, e = greeting(friends, date)
 
 	assert.Nil(t, e)
 
@@ -20,5 +23,19 @@ func TestAcceptance(t *testing.T) {
 		t,
 		mail,
 		"Subject: Happy birthday!\nHappy birthday, dear Mary!",
+		"mails shoulld be equals")
+}
+
+func TestAcceptance_JohnBirthday(t *testing.T) {
+	var date = time.Date(2024, 10, 1, 0, 0, 0, 0, time.Local)
+
+	var mail, e = greeting(friends, date)
+
+	assert.Nil(t, e)
+
+	assert.Equal(
+		t,
+		mail,
+		"Subject: Happy birthday!\nHappy birthday, dear John!",
 		"mails shoulld be equals")
 }
