872ed53eb226b9363d2dc547dd6a066b849fe95d
ntonjeta <ntonjeta@autistici.org>
2025-01-13 18:17:01 +0100

    add greeting sender interface fix assertion mock

--- birthday_greeting.go
+++ birthday_greeting.go
@@ -5,20 +5,26 @@ import (
 	"time"
 )
 
+type GreetingSender interface {
+	send(name string) error
+}
+
 func greeting(
 	friendRepository FriendRepository,
+	greetingSender GreetingSender,
 	date time.Time,
-) (string, error) {
+) error {
 
 	// TODO test error repository
 	var friends, _ = friendRepository.get()
 	for _, friend := range friends {
 		if friend.isBirthday(date) {
-			return fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!", friend.name), nil
+			greetingSender.send(friend.name)
+			// return fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!", friend.name), nil
 		}
 	}
 	// TODO no birthday test
-	return "", nil
+	return nil
 }
 
 func main() {
--- birthday_greeting_test.go
+++ birthday_greeting_test.go
@@ -18,39 +18,51 @@ type MockFriendRepository struct {
 }
 
 func (m *MockFriendRepository) get() ([]Friend, error) {
+
+	m.Called()
 	return friends, nil
+}
+
+type MockGreetingSender struct {
+	mock.Mock
+}
+
+func (m *MockGreetingSender) send(name string) error {
+	m.Called(name)
+	return nil
 }
 
 func TestAcceptance_MaryBirthday(t *testing.T) {
 	var date = time.Date(2024, 11, 21, 0, 0, 0, 0, time.Local)
 
-	testRepository := new(MockFriendRepository)
-	testRepository.On("get").Return(friends, nil)
+	mockFriendRepository := new(MockFriendRepository)
+	mockFriendRepository.On("get").Return(friends, nil)
 
-	var mail, e = greeting(testRepository, date)
+	mockGreetingSender := new(MockGreetingSender)
+	mockGreetingSender.On("send", "Mary").Return(nil)
+
+	var e = greeting(mockFriendRepository, mockGreetingSender, date)
 
 	assert.Nil(t, e)
 
-	assert.Equal(
-		t,
-		mail,
-		"Subject: Happy birthday!\nHappy birthday, dear Mary!",
-		"mails shoulld be equals")
+	mockFriendRepository.AssertExpectations(t)
+	mockGreetingSender.AssertExpectations(t)
+	// mockGreetingSender.AssertCalled(t, "send", "mary")
 }
 
 func TestAcceptance_JohnBirthday(t *testing.T) {
 	var date = time.Date(2024, 10, 1, 0, 0, 0, 0, time.Local)
 
-	testRepository := new(MockFriendRepository)
-	testRepository.On("get").Return(friends, nil)
+	mockFriendRepository := new(MockFriendRepository)
+	mockFriendRepository.On("get").Return(friends, nil)
 
-	var mail, e = greeting(testRepository, date)
+	mockGreetingSender := new(MockGreetingSender)
+	mockGreetingSender.On("send", "John").Return(nil)
+
+	var e = greeting(mockFriendRepository, mockGreetingSender, date)
 
 	assert.Nil(t, e)
 
-	assert.Equal(
-		t,
-		mail,
-		"Subject: Happy birthday!\nHappy birthday, dear John!",
-		"mails shoulld be equals")
+	mockFriendRepository.AssertExpectations(t)
+	mockGreetingSender.AssertExpectations(t)
 }
