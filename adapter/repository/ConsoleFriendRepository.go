8f33ccb7a8380bf176e112fa718a878482a17f8d
ntonjeta <ntonjeta@autistici.org>
2025-01-14 11:46:45 +0100

    minor

--- adapter/repository/ConsoleFriendRepository.go
+++ adapter/repository/ConsoleFriendRepository.go
@@ -3,14 +3,14 @@ package repository
 import (
 	"time"
 
-	g "github.com/ntonjeta/greeting"
+	"github.com/ntonjeta/greeting"
 )
 
 type ConsoleFriendRepository struct {
 }
 
-func (c *ConsoleFriendRepository) Get() ([]g.Friend, error) {
-	return []g.Friend{
+func (c *ConsoleFriendRepository) Get() ([]greeting.Friend, error) {
+	return []greeting.Friend{
 		{Name: "Mary", Surname: "Ann", Birthday: time.Date(1974, 11, 21, 0, 0, 0, 0, time.Local)},
 		{Name: "John", Surname: "Doe", Birthday: time.Date(1974, 10, 1, 0, 0, 0, 0, time.Local)},
 	}, nil
